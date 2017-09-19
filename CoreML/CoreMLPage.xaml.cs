20ccf689b4a35aef9c697b80cf35b7644574f50b
chomado <chomado@gmail.com>
2017-09-19 10:31:27 -0700

    Taking picture function

--- CoreML/CoreMLPage.xaml.cs
+++ CoreML/CoreMLPage.xaml.cs
@@ -16,6 +16,27 @@ namespace CoreML
         private async void Button_Clicked(object sender, EventArgs e)
         {
             // taking a picture
+            await CrossMedia.Current.Initialize();
+
+            var file = await CrossMedia.Current.TakePhotoAsync(options: new StoreCameraMediaOptions());
+
+            if (file == null)
+            {
+                return;
+            }
+
+            this.image.Source = ImageSource.FromStream(() => file.GetStream());
+
+            using (var fileStream = file.GetStream())
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await fileStream.CopyToAsync(memoryStream);
+                    var d = DependencyService.Get<Services.IDetectService>();
+                    var result = await d.DetectAsync(memoryStream.ToArray());
+                    await this.DisplayAlert(title: "result", message: result, cancel: "OK");
+                }
+            }
         }
     }
 }
