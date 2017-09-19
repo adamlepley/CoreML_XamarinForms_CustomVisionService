44990779f3c3b6a1307293f08265d2cb4704f360
chomado <chomado@gmail.com>
2017-09-19 10:51:59 -0700

    Add DetectService (empty yet)

---
+++ iOS/Services/DetectService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreML.Services;
+
+namespace CoreML.iOS.Services
+{
+    public class DetectService : IDetectService
+    {
+        public DetectService()
+        {
+        }
+
+        public Task<string> DetectAsync(byte[] image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
