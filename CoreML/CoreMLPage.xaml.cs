5770e9645d8548b84647fe4a68840ccc82e71312
chomado <chomado@gmail.com>
2017-09-19 10:19:17 -0700

    Taking picture button

--- CoreML/CoreMLPage.xaml.cs
+++ CoreML/CoreMLPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.IO;
+using Xamarin.Forms;
 
 namespace CoreML
 {
@@ -7,6 +11,11 @@ namespace CoreML
         public CoreMLPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            // taking a picture
         }
     }
 }
